486ee66efe2da12072100b6781800a91141bc9c9
Staobit <abdussomadidris@gmail.com>
2022-03-10 13:29:55 -0800

    idris commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- **/
-int main(void)
-{
-printf("Size of a char:%2d byte(s)\n", sizeof(char c));
-printf("Size of a int:%2d byte(s)\n", sizeof(int n));
-printf("Size of a long int:%2d byte(s)\n", sizeof(long int c));
-printf("Size of a long long int:%2d byte(s)\n", sizeof(long long int c));
-printf("Size of a float:%2d byte(s)", sizeof(float c));
-}
