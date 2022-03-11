dc716288225712d00edebee64139a6a8f246315c
Staobit <abdussomadidris@gmail.com>
2022-03-11 13:38:09 -0800

    idris commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {	
-	int n;
+        int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -17,7 +17,7 @@ int main(void)
 		printf("%i is positive\n", n);
         else if (n < 0)	
 		printf("%i is negative\n", n);
-	else  
+	else  	
 	        printf("%i is zero\n", n);
 	return (0);
-}	
+}
