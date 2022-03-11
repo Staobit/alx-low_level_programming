4b07afa6d4f66075d831fc03f8c91c1a4487a820
Staobit <abdussomadidris@gmail.com>
2022-03-11 12:24:44 -0800

    idris commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,3 +13,18 @@ int main(void)
 	return (0);
 }
 
+#include <stdio.h>
+
+int main () {
+
+	/* local variable definition */
+	int = n;
+
+	if (n > 0)
+		printf("%d is positive\n" , n);
+	else if (n == 0)
+		printf("%d is zero\n" , n);
+	else if (n < 0)
+		printf("%d is negative\n" , n);
+	return (0);
+
