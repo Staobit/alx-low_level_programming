cda479cd449bde4334e68d9de101d61da6c5acc5
Staobit <abdussomadidris@gmail.com>
2022-03-15 04:34:56 -0700

    idris commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,17 +6,16 @@
  * Return: Always 0 (Success)
  */
  int main(void)
- { 
-          putchar('_');
-	  putchar('p');
-	  putchar('u');
-	  putchar('t');
-	  putchar('c');
-	  putchar('h');
-	  putchar('a');
-	  putchar('r');
-	  putchar('\n');
+{ 
+      	putchar('_');
+        putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
 	  return (0);
-
-	  }	  
+}	  
