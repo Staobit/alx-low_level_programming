1a59c46a7042bd54d4a4be6c328ee07198b38bcd
Staobit <abdussomadidris@gmail.com>
2022-03-21 12:51:48 -0700

    idris commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 include "main.h"
 
 /**
- * _puts - a function that prints a string,
+ * puts - a function that prints a string,
  * followed by a new line, to stdout
  * @str: string to be printed
  */
